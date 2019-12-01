22d6d0ba50fb50c35893d9345c96e238d7ae13ed
Алексей Судавцов <alexey@familytrain.team>
2019-12-01 19:02:36 +0300

    feat: build path, api url

--- src/overmind/api/effects.ts
+++ src/overmind/api/effects.ts
@@ -1,21 +1,26 @@
 import ky from 'ky';
 import { request, response } from './types';
 
+const requestUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://35.195.25.70/api.php'
+    : '/api.php';
+
 export async function logIn(
   login: string,
   password: string
 ): Promise<response> {
   return ky
-    .post('/api.php', {
+    .post(requestUrl, {
       json: { action: request.login, login, password }
     })
     .json();
 }
 
 export async function getQuotes(): Promise<response> {
-  return ky.post('/api.php', { json: { action: request.quote } }).json();
+  return ky.post(requestUrl, { json: { action: request.quote } }).json();
 }
 
 export async function getHistory(): Promise<response> {
-  return ky.post('/api.php', { json: { action: request.history } }).json();
+  return ky.post(requestUrl, { json: { action: request.history } }).json();
 }
