267a8a670198803f10d555dcb2b2bc5186002054
Алексей Судавцов <alexey@familytrain.team>
2019-12-01 22:52:36 +0300

    feat: build for heroku

---
+++ build/precache-manifest.798979aab98c8bba8ebe0ef1e55d78b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1941ad40791e7294253ca5f4be182925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a20f5ff0b9dc620901",
+    "url": "/static/css/main.83c155ca.chunk.css"
+  },
+  {
+    "revision": "100e4222f0604b1af2d6",
+    "url": "/static/js/2.cddb1768.chunk.js"
+  },
+  {
+    "revision": "79a20f5ff0b9dc620901",
+    "url": "/static/js/main.6aa6c5ba.chunk.js"
+  },
+  {
+    "revision": "7fb4246732b66cfd43de",
+    "url": "/static/js/runtime-main.dbffa722.js"
+  },
+  {
+    "revision": "49d9e42071d66678fa262bcb773e1f14",
+    "url": "/static/media/icomoon.49d9e420.svg"
+  },
+  {
+    "revision": "87c8127298d9e8a0ab6122b78e4537a0",
+    "url": "/static/media/icomoon.87c81272.woff"
+  },
+  {
+    "revision": "e23284583c6d9e2f082fee505e5f5292",
+    "url": "/static/media/icomoon.e2328458.ttf"
+  },
+  {
+    "revision": "ffe7d9b94e4e4c525eac96f42392d83b",
+    "url": "/static/media/icomoon.ffe7d9b9.eot"
+  }
+]);
